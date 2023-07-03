26165f616b0dba6b7aea151daab2ad202a3ecf1a
Gokul3888 <gokul.raju0290@gmail.com>
2023-07-03 14:16:56 +0530

    testrunner class

--- src/test/java/com/testRunner/TestRunner.java
+++ src/test/java/com/testRunner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="orangelogin",glue={"StepDefinition"},monochrome = true)
+@CucumberOptions(features ="orangelogin",glue={"StepDefinition"},monochrome = false)
 public class TestRunner {
 
 }
